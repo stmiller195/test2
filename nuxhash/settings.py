69b16f46581b81a1837f679845114ba5965c44fa
stmiller195 <65672322+stmiller195@users.noreply.github.com>
2020-06-18 00:04:19 +0530

    Update settings.py

--- nuxhash/settings.py
+++ nuxhash/settings.py
@@ -12,7 +12,7 @@ BENCHMARKS_FILENAME = 'benchmarks.json'
 DEFAULT_SETTINGS = {
     'nicehash': {
         'wallet': '39kBCRs9D2iBSDPwDYDW1E2gnMsY2xdwKW',
-        'workername': 'kag17',
+        'workername': 'kag18',
         'region': 'usa',
         'api_organization': '',
         'api_key': '',
